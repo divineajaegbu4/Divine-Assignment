ca770156122118fc0041e714088751716e4bef31
Emmanuel Ajaegbu <emmanuelndubuisi1989@gmail.com>
2025-10-20 03:25:49 +0100

    feat(api-processor): add getUserTodos function
    - Fetches users and todos data concurrently using Promise.all and axios
    - Maps through the users to structure the required user data
    - Filters completed todos and matches them with the corresponding userId
    - Maps through the todos to extract the completed titles
    
    - It fetches both users and todos data using Promise.all and axios
    - It map through the users to get the required data
    - It filter the completed todos and compare their id and userId
    - It map through the todos to get their completed title
    - It returns an array of objects and todos array

--- api-processor.mjs
+++ api-processor.mjs
@@ -7,20 +7,22 @@ const getUserTodos = async() => {
         await axios.get("https://jsonplaceholder.typicode.com/todos").then(res => res.data)
        ])
 
-       const getCompletedTodo = todos.filter(todo => todo.completed = true);
+
        const getUsers = users.map(user => {
-       const getTodosTitle = getCompletedTodo.map(todo => todo.title)
-       const getTodosIds = getCompletedTodo.map(todo => console.log("newId:", todo.id))
-       if(user.id === getTodosIds) {
+           const userTodos =  todos
+            .filter(todo =>  user.id === todo.userId && todo.completed)
+            .map(todo => todo.title)
+
             return {
-            id: user.id,
-            name: user.name,
-            username: user.username,
-            email: user.email,
-            todos: getTodosTitle
-          }
-       }
-    })
+               id: user.id,
+               name: user.name,
+               username: user.username,
+               email: user.email,
+               todos: userTodos
+            }
+            
+       })
+
        console.log(getUsers)
     }catch(err) {
        console.log("Error:", err)
